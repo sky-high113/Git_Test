c34e6d60381a5d49e10e1eb50913e74ee2373de9
sunghak.kim <rlatjdgkr113@gmail.com>
2026-09-08 12:01:36 +0900

    main.py 함수화 desc : 기존 기능 함수화 진행 name : sunhak.kim

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-# print 0 ~ 9 
-for i in range(10):
-    print(i)
+
+def func_print():
+    # print 0 ~ 9 
+    for i in range(10):
+        print(i)
+        
+    print("Let's go Home!!")
     
-print("Let's go Home!!")
+    
+func_print()
